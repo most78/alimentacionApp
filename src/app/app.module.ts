f3c4ad2203a43db776a87138d50a88f101d85386
Marcos Ostos <marcos.ostos@gmail.com>
2018-04-16 20:44:34 +0200

    inject constructor on export class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpClient } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./components/header/header.component";
 
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,4 +19,12 @@ import { HeaderComponent } from "./components/header/header.component";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
